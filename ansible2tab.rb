73c9600c414583c46f7ce782d030d7b065e813b8
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2017-12-13 00:37:18 +0900

    Version 0.0.3

--- ansible2tab.rb
+++ ansible2tab.rb
@@ -1,9 +1,9 @@
 class Ansible2tab < Formula
   desc "Convert output of ansible command to TSV/JSON/Markdown/Backlog"
   homepage "https://github.com/muziyoshiz/ansible2tab"
-  url "https://github.com/muziyoshiz/ansible2tab/releases/download/v0.0.2/ansible2tab_darwin_amd64.zip"
-  version "v0.0.2"
-  sha256 "1d765e1c2f97e4ff03da4e47c6440643eae5cf01dee61de5ccf8c985711edff6"
+  url "https://github.com/muziyoshiz/ansible2tab/releases/download/v0.0.3/ansible2tab_darwin_amd64.zip"
+  version "v0.0.3"
+  sha256 "v5c5bbf8fbf7a157d5637609492580c95844a4d1265b83e17aef50dccc28a9b26"
 
   def install
     bin.install 'ansible2tab'
