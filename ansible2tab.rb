09d6eac5369834e207f7be0bcdbbcd23d1e80e08
Masahiro Yoshizawa <muziyoshiz@gmail.com>
2017-12-13 00:45:15 +0900

    Version 0.0.3

--- ansible2tab.rb
+++ ansible2tab.rb
@@ -3,7 +3,7 @@ class Ansible2tab < Formula
   homepage "https://github.com/muziyoshiz/ansible2tab"
   url "https://github.com/muziyoshiz/ansible2tab/releases/download/v0.0.3/ansible2tab_darwin_amd64.zip"
   version "v0.0.3"
-  sha256 "273df2a2fc9a0ed9323cc66da4bf8349d023e1c6fd7d265d52f6f623582cf333"
+  sha256 "a027be36c5c1b6a3167b0f938e5141aca32c1e5fb20f164c33e8e39ab9b47f99"
 
   def install
     bin.install 'ansible2tab'
